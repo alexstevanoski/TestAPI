5b9424c69002c21a94af72e1efdaddfd60262e5a
alexstevanoski <alexanderleigh89@gmail.com>
2023-10-09 22:55:26 +1100

    Opened Project

---
+++ TestAPI/Data.cs
@@ -0,0 +1,6 @@
+namespace TestAPI;
+public class Item
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ TestAPI/Program.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+[Route("api/[controller]")]
+[ApiController]
+
+namespace TestAPI;
+
+public class ItemsController : ControllerBase
+{
+    private readonly 
+}
