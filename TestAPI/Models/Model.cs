4e9543d135d1fe05a8eea1672b90ece1825b08cf
alexstevanoski <alexanderleigh89@gmail.com>
2023-10-17 14:45:36 +1100

    rebult project

--- TestAPI/Models/Model.cs
+++
@@ -1,9 +0,0 @@
-namespace TestAPI.Models
-{
-    public class TestAPIClass
-    {
-        public int Id { get; set;}
-        public string Name {get ;set;}
-        public string Description {get ;set;}
-    }
-}
--- TestAPI/Program.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-using TestAPI.Data;
-using TestAPI.Models;
-
-builder.Services.AddDbContext<ApiContext>
-    (opt => opt.UseInMemoryDatabase("ApiDb"));
-
